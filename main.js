eed7be9f70cacb840088dbd7a7b3c2d16b2e9762
MEsp <martin.m.espericueta@gmail.com>
2020-06-07 14:15:10 -0700

    Finish buttons for delete and edit, finish project. Next step add db for save and show task when the page start

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    $(".add").click(add_task);
-    //document.queryselector(".add").addeventlistener("click", agregar);
-
+    document.querySelector(".add").addEventListener("click", add_task, true);
+    var texto = document.querySelector("#task-input");
     var task_container = $(".tasks");
-    console.log(task_container[0]);
-    var i = 0;
+    var i = 1;
 
     function add_task() {
-        var texto = document.querySelector("#task-input").value;
-        if (texto == "")
+        if (texto.value == "")
             $(".error").show();
         //document.querySelector(".error").style.display = "flex";
         else {
@@ -17,46 +14,73 @@ document.addEventListener("DOMContentLoaded", () => {
             //document.querySelector(".error").style.display = "none";
             var cont = document.createElement("div");
             cont.setAttribute("class", "task");
-            cont.setAttribute("id", "task" + 1);
+            cont.setAttribute("id", "task" + i);
+            cont.setAttribute("data-id", i);
             document.querySelector(".tasks").appendChild(cont);
 
             var descripcion = document.createElement("p");
-            descripcion.setAttribute("id", "desciption" + 1);
-            descripcion.textContent = texto;
+            descripcion.setAttribute("id", "description" + i);
+            descripcion.textContent = texto.value;
             cont.appendChild(descripcion);
 
             var button = document.createElement("button");
-            button.setAttribute("id", "update" + 1);
+            button.setAttribute("id", "update" + i);
             button.setAttribute("class", "btn btn-warning");
-            button.setAttribute("data-id", 1);
+            button.setAttribute("data-id", i);
+            button.addEventListener("click", () => {
+                var id = button.getAttribute("data-id");
+                edit_task(id);
+            });
             cont.appendChild(button);
             var icon = document.createElement("i");
             icon.setAttribute("class", "fas fa-pen");
             button.appendChild(icon);
-            button.addEventListener("click", update_task);
+
 
             button = document.createElement("button");
-            button.setAttribute("id", "delete" + 1);
+            button.setAttribute("id", "delete" + i);
             button.setAttribute("class", "btn btn-danger");
-            button.setAttribute("data-id", 1);
+            button.setAttribute("data-id", i);
             cont.appendChild(button);
             icon = document.createElement("i");
             icon.setAttribute("class", "fas fa-trash-alt");
             button.appendChild(icon);
-            button.addEventListener("click", delete_task);
+            button.addEventListener("click", () => {
+                var id = button.getAttribute("data-id");
+                delete_task(id);
+            });
+
             i++;
+            document.querySelector("#task-input").value = "";
         }
     }
 
-    function update_task() {
-        console.log("update");
+    function edit_task(id) {
+        var text = document.querySelector("#description" + id).textContent;
+        texto.value = text;
+        var add = document.querySelector(".add");
+        add.setAttribute("class", "btn btn-warning add");
+        add.setAttribute("data-id", id);
+        add.textContent = "Update";
+        add.removeEventListener("click", add_task, true);
+        add.addEventListener("click", confirmUpdate, true);
     }
 
     function confirmUpdate() {
-
+        var id = document.querySelector(".add").getAttribute("data-id");
+        var task = document.querySelector("#description" + id);
+        task.textContent = texto.value;
+        var add = document.querySelector(".add");
+        add.setAttribute("class", "btn btn-success add");
+        add.removeAttribute("data-id");
+        add.textContent = "Add";
+        add.addEventListener("click", add_task, true);
+        add.removeEventListener("click", confirmUpdate, true);
+        texto.value = "";
     }
 
-    function delete_task() {
-        console.log("delete");
+    function delete_task(id) {
+        var task = document.querySelector("#task" + id);
+        task.remove();
     }
 });
